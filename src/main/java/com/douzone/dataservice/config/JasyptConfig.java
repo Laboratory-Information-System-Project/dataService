d73049cd13abc3842b61e6a502c964fb1da5f13d
liverbird <ehdgns07@gmail.com>
2022-12-05 11:02:17 +0900

    docker property modify

---
+++ src/main/java/com/douzone/dataservice/config/JasyptConfig.java
@@ -0,0 +1,47 @@
+package com.douzone.dataservice.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptConfig {
+
+    @Bean(name = "jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+
+        String key = "my_jasypt_key";
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(key); // 암호화할 때 사용하는 키
+        config.setAlgorithm("PBEWithMD5AndDES"); // 암호화 알고리즘
+        config.setKeyObtentionIterations("1000"); // 반복할 해싱 회수
+        config.setPoolSize("1"); // 인스턴스 pool
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator"); // salt 생성 클래스
+        config.setStringOutputType("base64"); //인코딩 방식
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+
+    @Bean
+    public void printId(){
+        System.out.println(jasyptEncoding("jdbc:mysql://lis-project.cwjfch0v8vje.ap-northeast-2.rds.amazonaws.com/data?allowMultiQueries=true"));
+        System.out.println(jasyptEncoding("root"));
+        System.out.println(jasyptEncoding("12345678"));
+
+
+    }
+
+    public String jasyptEncoding(String value) {
+
+        String key = "my_jasypt_key";
+        StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
+        pbeEnc.setAlgorithm("PBEWithMD5AndDES");
+        pbeEnc.setPassword(key);
+        return pbeEnc.encrypt(value);
+    }
+}
